3d38f7f84c9f1352db3a98002924ca7370732ed3
unknown <enesbc46@gmail.com>
2020-10-05 21:35:26 +0300

    private info

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const { User } = require("./models/user");
+const key = require("./config/key");
+const app = express();
 mongoose
-  .connect(
-    "mongodb+srv://enes123:enes123@boilerplate.ifzvm.mongodb.net/Boilerplate?retryWrites=true&w=majority",
-    { useNewUrlParser: true, useUnifiedTopology: true }
-  )
+  .connect(key.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
   .then(() => console.log("DB connected"))
   .catch((err) => console.error(err));
 
-const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.status(200).send("Selamlar aq");
+app.post("/api/users/register", (req, res) => {
+  const user = new User(req.body);
+  user
+    .save()
+    .then(() => res.status(200).json(`User '${req.body.name}' added!`))
+    .catch((err) => res.status(400).json(`Unable to add user. Error: ${err}.`));
 });
 
-app.listen(5000);
+app.listen(5000, () => console.log(`App listening on ${5000}`));
